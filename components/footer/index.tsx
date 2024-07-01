bfb63580e3a68abe4920c3db832804d2d1a60693
Omina Abdullayeva <ominaabdullayeva76@gmail.com>
2024-07-01 10:05:06 +0500

    ...

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -12,9 +12,10 @@ const Footer = () => {
   useEffect(() => {
     getAll({ page: 1, limit: 100, search: "" });
   }, []);
+
   return (
     <>
-      <div className="bg-white rounded-lg p-[60px] h-[400px] flex justify-between">
+      <div className="bg-white rounded-lg p-6 sm:p-10 md:p-12 lg:p-16 h-auto flex flex-col md:flex-row justify-between gap-6">
         <div className="flex-1">
           <Link href="/" className="flex items-center gap-1 text-[14px]">
             <Image
@@ -22,11 +23,13 @@ const Footer = () => {
               alt="logo"
               width={61}
               height={61}
-              className="w-[61px]"
+              className="w-[31px] md:w-[45px] lg:w-[61px]"
             />
-            <b className="text-[#d25300] font-extrabold text-[33px]">EXNOARK</b>
+            <b className="text-[#d25300] font-extrabold text-[20px] md:text-[24px] lg:text-[33px]">
+              EXNOARK
+            </b>
           </Link>
-          <b className="text-[18px] mt-8 block mb-5">
+          <b className="text-[16px] sm:text-[18px] mt-8 block mb-5">
             Bizni ijtimoiyi tarmoqlar
           </b>
           <div className="flex gap-3">
@@ -34,7 +37,7 @@ const Footer = () => {
               <a
                 key={index}
                 href={item.link}
-                className="flex items-center justify-center gap-[4px] bg-[#f0f0f0] py-[13px] px-[14px] rounded-lg"
+                className="flex items-center justify-center gap-[4px] bg-[#f0f0f0] py-2 px-2 sm:py-[13px] sm:px-[14px] rounded-lg"
               >
                 {item.icon}
               </a>
@@ -43,7 +46,9 @@ const Footer = () => {
         </div>
 
         <ul className="text-gray-400 text-[14px] flex-1 flex flex-col gap-2">
-          <b className="text-[18px] text-black mb-3 block">Tashkilot haqida</b>
+          <b className="text-[16px] sm:text-[18px] text-black mb-3 block">
+            Tashkilot haqida
+          </b>
           <li className="hover:text-black cursor-pointer">Texnoark haqida</li>
           <li className="hover:text-black cursor-pointer">
             Muddatli to&apos;lov
@@ -58,27 +63,33 @@ const Footer = () => {
         </ul>
 
         <ul className="text-gray-400 text-[14px] flex-1 flex flex-col gap-2">
-          <b className="text-[18px] text-black mb-4 block">Kategoriya</b>
+          <b className="text-[16px] sm:text-[18px] text-black mb-3 block">
+            Kategoriya
+          </b>
           {categoriesData.map((item, index) => (
-            <li className="hover:text-black cursor-pointer">{item.name}</li>
+            <li key={index} className="hover:text-black cursor-pointer">
+              {item.name}
+            </li>
           ))}
         </ul>
 
         <ul className="text-gray-500 text-[14px] flex-1">
-          <b className="text-[18px] text-black">Biz bilan aloqa</b>
-          <li className="flex items-center gap-5 mt-4">
+          <b className="text-[16px] sm:text-[18px] text-black">
+            Biz bilan aloqa
+          </b>
+          <li className="flex items-center gap-2 sm:gap-5 mt-4">
             {locationIcon} 100052, O&apos;zbekiston Respublikasi, Toshkent
             shahri, Bodomzor yo&apos;li 1-tor ko&apos;chasi, 72
           </li>
-          <li className="flex items-center gap-5 mt-2">
+          <li className="flex items-center gap-2 sm:gap-5 mt-2">
             {phoneIcon} +998 71 300 30 30
           </li>
-          <li className="flex items-center gap-5 mt-2">
+          <li className="flex items-center gap-2 sm:gap-5 mt-2">
             {emailIcon} texnoarko@gmail.com
           </li>
         </ul>
       </div>
-      <div className="text-gray-500 my-3 text-[12px]">
+      <div className="text-gray-500 my-3 text-[12px] text-center">
         &copy; 2022 Ashyo ro&apos;yxatdan o&apos;tgan litsenzalangan bu brend.
       </div>
     </>

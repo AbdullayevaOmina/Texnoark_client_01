3b0b4ecbf301bbf69a90a29a84455db3c9c5839c
Omina Abdullayeva <ominaabdullayeva76@gmail.com>
2024-07-08 08:40:17 +0500

    ..

--- service/product.service.ts
+++ service/product.service.ts
@@ -4,7 +4,7 @@ import { GetAll } from "@/types/product-types";
 export const getAll = async (data: GetAll) => {
   try {
     const response = await http.get(
-      `/products?page=${data.page}&limit=${data.limit}&name=${data.name}`
+      `/products?page=${data.page}&limit=${data.limit}&name=${data.search}`
     );
     return response.data.products;
   } catch (error) {
